f405068c7b147bd0c609dc3142942042dc5156ea
Dan Rudolf <danrudolf@gmail.com>
2014-05-22 13:28:16 -0500

    Initial Commit

---
+++ Breakout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Breakout
+//
+//  Created by dan rudolf on 5/22/14.
+//  Copyright (c) 2014 Dan Rudolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Breakout/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Breakout
+//
+//  Created by dan rudolf on 5/22/14.
+//  Copyright (c) 2014 Dan Rudolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
